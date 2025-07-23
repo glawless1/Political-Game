3aa72ae5948c67472fca16495766e5d1abef4dce
glawless1 <lawless.george@yahoo.com>
2025-07-23 12:02:27 +0100

    trying button

--- Assets/Scripts/StartButtonHandler.cs
+++ Assets/Scripts/StartButtonHandler.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Add this!
 
 public class StartButtonHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Call this from the button's OnClick event
+    public void OnStartButtonClicked()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Loads the next scene in the build order
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
