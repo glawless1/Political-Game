37ad08abf8715cdcba62f6fc11acd5af745b6c19
glawless1 <lawless.george@yahoo.com>
2025-07-25 19:47:49 +0100

    Added AIAgent file for development

--- Assets/Scripts/StartButtonHandler.cs
+++ Assets/Scripts/StartButtonHandler.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // Add this!
+using UnityEngine.SceneManagement;
 
 public class StartButtonHandler : MonoBehaviour
 {
     // The name of the scene to load
-    [SerializeField] private string nextGameLevel = "NewsLoading";  // Fixed declaration
+    [SerializeField] private string nextGameLevel = "NewsLoading";
 
     // Function for button clicking
     public void OnStartButtonClicked()
